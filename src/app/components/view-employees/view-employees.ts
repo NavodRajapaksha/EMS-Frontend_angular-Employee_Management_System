b06af3b755ae374e85b752fa8cba68a3a7ff1671
Navod D Rajapaksha <129612778+NavodRajapaksha@users.noreply.github.com>
2026-03-25 00:47:54 +0530

    fix: view-emplyee data loading errors

--- src/app/components/view-employees/view-employees.ts
+++ src/app/components/view-employees/view-employees.ts
@@ -1,27 +1,25 @@
-import { Component } from '@angular/core';
-import { OnInit } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { NavBar } from "../nav-bar/nav-bar";
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-view-employees',
-  imports: [NavBar],
+  imports: [NavBar, RouterLink, CommonModule],
   templateUrl: './view-employees.html',
   styleUrl: './view-employees.css',
 })
 export class ViewEmployees implements OnInit {
 
-  public employeeArray: any[] = [];
+  public employeeArray = signal<any[]>([]);
 
-  constructor(private http: HttpClient) {
-    console.log('ViewEmployees component initialized');
-   }
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-    this.http.get('http://localhost:8080/all').subscribe((data) =>{
-      this.employeeArray = data as any[];
-      console.log('Employee data fetched:', this.employeeArray);
-    })
+    this.http.get('http://localhost:8080/employee/all').subscribe((data) => {
+      this.employeeArray.set(data as any[]);
+      console.log('Employee data fetched:', this.employeeArray());
+    });
   }
-
 }
